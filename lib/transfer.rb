7d2006576028edd1222ab774a7278bda8e9e1aba
mkoenke <mkoenke@sas.upenn.edu>
2020-10-09 09:52:47 -0400

    Bank account working

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,22 @@
 class Transfer
   # your code here
+
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize (sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid? 
+  end
+
+  def execute_transaction
+  end
+
+  def reverse_transfer
+  end
+
 end
